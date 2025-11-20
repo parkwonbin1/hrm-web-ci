3aefacf13cd2f220eef317dae6c9b61ceb3146fa
parkwonbin1 <qkrdnjs1219@gmail.com>
2025-11-20 11:52:59 +0900

    Add Composer install step to Dockerfile

--- web/Dockerfile.php
+++ web/Dockerfile.php
@@ -1,12 +1,19 @@
 FROM php:8.1-fpm-bullseye
 
-RUN docker-php-ext-install mysqli pdo pdo_mysql
+RUN apt-get update && apt-get install -y \
+    git \
+    unzip \
+    zip \
+    && docker-php-ext-install mysqli pdo pdo_mysql
+
+COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
 
 WORKDIR /var/www/html
 
 COPY --chown=www-data:www-data . .
 
 USER www-data
+RUN composer install --no-dev --optimize-autoloader
 
 EXPOSE 9000
 
